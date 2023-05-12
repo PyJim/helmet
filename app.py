b9ef44c9753c247cb55e8889622df4470d53955e
Zatey <michael.zatey@kibo.school>
2023-05-11 19:31:29 -0700

    other endpoints

--- app.py
+++ app.py
@@ -7,6 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/disaster_watch')
+def disaster_watch():
+    return render_template('disaster_watch.html')
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
+@app.route('/donate')
+def donate():
+    return render_template('Donate.html')
+
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+@app.route('/notification')
+def notification():
+    return render_template('notification.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
