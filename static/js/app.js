275965940b1178c5871e15f223f26750f1c95c20
Essel Jimmy Kodwo <jimmy.essel@kibo.school>
2023-06-14 14:04:13 +0000

    button functions

--- static/js/app.js
+++ static/js/app.js
@@ -19,3 +19,22 @@ nav_links.forEach(link => {
     });
 });
 
+const create_new_post = document.getElementById("create_new_post");
+create_new_post.addEventListener("click", function(){
+    let create_post = document.getElementById("create_post");
+    create_post.classList.toggle("active");
+});
+
+const create_new_event = document.getElementById("create_new_event");
+create_new_event.addEventListener("click", function(){
+    let create_event = document.getElementById("create_event");
+    create_event.classList.toggle("active");
+});
+
+const submit_button = document.getElementById("submit-button");
+submit_button.addEventListener("click", function(){
+    let create_post = document.getElementById("create_post");
+    create_post.classList.remove("active");
+    let create_event = document.getElementById("create_event");
+    create_event.classList.remove("active");
+});
